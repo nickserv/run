78311c558e68bc028b7fcf819e1f7fcc50ed7dca
Nicolas McCurdy <thenickperson@gmail.com>
2014-02-25 03:12:34 -0500

    Throw RuntimeErrors for invalid/missing extensions and when no files are given

--- lib/run.rb
+++ lib/run.rb
@@ -5,11 +5,18 @@ module Run
   COMMANDS = YAML.load_file('lib/commands.yml')
 
   def self.start(args)
+    fail RuntimeError, 'No files given.' if args == []
     path = args[0]
     extension = File.extname path
 
-    if extension != ''
+    if extension == ''
+      fail RuntimeError, 'Run could not determine how to run this file ' \
+                         'because it has no extension.'
+    else
       extension.sub!('.', '')
+      fail RuntimeError, 'Run could not determine how to run this file ' \
+                         'because it has an unknown extension.' \
+                         unless COMMANDS.key? extension
       system COMMANDS[extension].gsub('%', path)
     end
   end
--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -6,11 +6,23 @@ describe Run do
   end
 
   describe '.start' do
-    context 'when a valid filename is given with a known extension' do
+    context 'when a filename is given with a known extension' do
       it 'runs the file' do
         expect(Run).to receive('system').with('ruby hello.rb')
         Run.start ['hello.rb']
       end
+    end
+
+    context 'when a filename is given without a known extension' do
+      it { expect{ Run.start ['hello.unknown'] }.to raise_error RuntimeError }
+    end
+
+    context 'when a filename is given without any extension' do
+      it { expect{ Run.start ['hello'] }.to raise_error RuntimeError }
+    end
+
+    context 'when no filename is given' do
+      it { expect{ Run.start [] }.to raise_error RuntimeError }
     end
   end
 end
