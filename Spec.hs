98d765d670504dc0ed13d959eb19e787f0f6ec14
Nicolas McCurdy <thenickperson@gmail.com>
2014-02-28 15:57:00 -0500

    Fix some hlint issues

--- Spec.hs
+++ Spec.hs
@@ -5,14 +5,14 @@ main = hspec $ do
   describe "Run" $ do
     describe ".command_for_file" $ do
       context "when a filename is given with a known extension" $ do
-        it "should be a valid command" do
-          (Run.commandForFile "hello.rb") `shouldBe` Just "ruby hello.rb"
+        it "should be a valid command" $ do
+          Run.commandForFile "hello.rb" `shouldBe` Just "ruby hello.rb"
 
       context "when a filename is given without a known extension" $ do
-        (Run.command_for_file "hello.unknown") `shouldBe` Nothing
+        Run.command_for_file "hello.unknown" `shouldBe` Nothing
 
       context "when a filename is given without any extension" $ do
-        (Run.command_for_file "hello") `shouldBe` Nothing
+        Run.command_for_file "hello" `shouldBe` Nothing
 
     describe ".start" $ do
       context "when a filename is given with a known extension" $ do
@@ -21,10 +21,10 @@ main = hspec $ do
           Run.start ["hello.rb"]
 
       context "when a filename is given without a known extension" $ do
-        (Run.start ["hello.unknown"]) `shouldThrow` anyException
+        Run.start ["hello.unknown"] `shouldThrow` anyException
 
       context "when a filename is given without any extension" $ do
-        (Run.start ["hello"]) `shouldThrow` anyException
+        Run.start ["hello"] `shouldThrow` anyException
 
       context "when no filename is given" $ do
-        (Run.start []) `shouldThrow` anyException
+        Run.start [] `shouldThrow` anyException
