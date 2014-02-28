477f09c012808e0afcd666b3eac77747cccd4282
Nicolas McCurdy <thenickperson@gmail.com>
2014-02-27 22:01:57 -0500

    Extract a new .command_for_file method

--- lib/run.rb
+++ lib/run.rb
@@ -4,19 +4,21 @@ require 'yaml'
 module Run
   COMMANDS = YAML.load_file('lib/commands.yml')
 
+  def self.command_for_file(path)
+    extension = File.extname(path).sub('.', '')
+
+    COMMANDS[extension].gsub('%', path) if COMMANDS.key?(extension)
+  end
+
   def self.start(args)
     fail 'No files given.' if args == []
-    path = args[0]
-    extension = File.extname path
+    command = command_for_file args[0]
 
-    if extension == ''
-      fail 'Run could not determine how to run this file because it has no ' \
-           'extension.'
+    if command.nil?
+      fail 'Run could not determine how to run this file because it does ' \
+           'not have a known extension.'
     else
-      extension.sub!('.', '')
-      fail 'Run could not determine how to run this file because it has an ' \
-           'unknown extension.' unless COMMANDS.key? extension
-      system COMMANDS[extension].gsub('%', path)
+      system command
     end
   end
 end
--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -5,6 +5,22 @@ describe Run do
     expect(Run::VERSION).to_not be_nil
   end
 
+  describe '.command_for_file' do
+    context 'when a filename is given with a known extension' do
+      it 'should be a valid command' do
+        expect(Run.command_for_file 'hello.rb').to eq 'ruby hello.rb'
+      end
+    end
+
+    context 'when a filename is given without a known extension' do
+      it { expect(Run.command_for_file 'hello.unknown').to be_nil }
+    end
+
+    context 'when a filename is given without any extension' do
+      it { expect(Run.command_for_file 'hello').to be_nil }
+    end
+  end
+
   describe '.start' do
     context 'when a filename is given with a known extension' do
       it 'runs the file' do
