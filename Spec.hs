0ad1351a444e87a50dd308a38e1fc7bfea05a4b7
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-01 14:23:19 -0500

    Remove redundant "do"s from the spec

--- Spec.hs
+++ Spec.hs
@@ -1,30 +1,30 @@
 import Test.Hspec
 
 main :: IO ()
-main = hspec $ do
-  describe "Run" $ do
-    describe ".commandForFile" $ do
-      context "when a filename is given with a known extension" $ do
-        it "should be a valid command" $ do
+main = hspec $
+  describe "Run" $
+    describe ".commandForFile" $
+      context "when a filename is given with a known extension" $
+        it "should be a valid command" $
           Run.commandForFile "hello.rb" `shouldBe` Just "ruby hello.rb"
 
-      context "when a filename is given without a known extension" $ do
+      context "when a filename is given without a known extension" $
         Run.commandForFile "hello.unknown" `shouldBe` Nothing
 
-      context "when a filename is given without any extension" $ do
+      context "when a filename is given without any extension" $
         Run.commandForFile "hello" `shouldBe` Nothing
 
-    describe ".start" $ do
-      context "when a filename is given with a known extension" $ do
-        it "runs the file" $ do
+    describe ".start" $
+      context "when a filename is given with a known extension" $
+        it "runs the file" $
           --expect(Run).to receive('system').with('ruby hello.rb')
           Run.start ["hello.rb"]
 
-      context "when a filename is given without a known extension" $ do
+      context "when a filename is given without a known extension" $
         Run.start ["hello.unknown"] `shouldThrow` anyException
 
-      context "when a filename is given without any extension" $ do
+      context "when a filename is given without any extension" $
         Run.start ["hello"] `shouldThrow` anyException
 
-      context "when no filename is given" $ do
+      context "when no filename is given" $
         Run.start [] `shouldThrow` anyException
