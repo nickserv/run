4fe92d4b1bd1be36f0b77671c37c142b8202f4e0
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-01 13:24:22 -0500

    Start implementing the Haskell port

---
+++ Main.hs
@@ -0,0 +1,28 @@
+import System.Environment
+import System.IO
+import System.Process
+
+commands :: [(String, String)]
+commands = [ ("c", "gcc % && ./a.out"),
+             ("clj", "clj %"),
+             ("coffee", "coffee %"),
+             ("go", "go run %"),
+             ("hs", "runhaskell %"),
+             ("jar", "java -jar %"),
+             ("lua", "lua %"),
+             ("ml", "ocaml %"),
+             ("py", "python %"),
+             ("rb", "ruby %"),
+             ("sh", "sh %"),
+             ("zsh", "zsh %") ]
+
+commandForExtension :: String -> Maybe String
+commandForExtension extension = lookup extension commands
+
+main = do
+         args <- System.Environment.getArgs
+         case commandForExtension (head args) of
+           Just command -> do
+             System.Process.runCommand command 
+             return ()
+           Nothing -> hPutStrLn stderr "Error: Extension not found"
