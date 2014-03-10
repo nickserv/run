603a79f97fc2fadaf7feb2c8c3ed699d0f387e1b
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-10 12:10:52 -0400

    Use Describe() instead of Context() in the specs

--- run_test.go
+++ run_test.go
@@ -10,19 +10,19 @@ func Test(t *testing.T) {
 
   g.Describe("Run", func() {
     g.Describe(".command_for_file", func() {
-      g.Context("when a filename is given with a known extension", func() {
+      g.Describe("when a filename is given with a known extension", func() {
         g.It("should be a valid command", func() {
           g.Assert(Run.command_for_file("hello.rb")).Equal("ruby hello.rb")
         })
       })
 
-      g.Context("when a filename is given without a known extension", func() {
+      g.Describe("when a filename is given without a known extension", func() {
         g.It("should be nil", func() {
           g.Assert(Run.command_for_file("hello.unknown")).Equal(nil)
         })
       })
 
-      g.Context("when a filename is given without any extension", func() {
+      g.Describe("when a filename is given without any extension", func() {
         g.It("should be nil", func() {
           g.Assert(Run.command_for_file("hello")).Equal(nil)
         })
