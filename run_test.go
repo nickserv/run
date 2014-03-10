d908dfcc440a3db9edf8c231d70112b73601975e
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-10 19:28:45 -0400

    Assign a variable for checking against extension errors in the specs

--- run_test.go
+++ run_test.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+  . "github.com/franela/goblin"
+
   "errors"
   "testing"
-  . "github.com/franela/goblin"
 )
 
 func Test(t *testing.T) {
   g := Goblin(t)
 
   g.Describe("Run", func() {
+    extensionError := errors.New("run could not determine how to run this file because it does not have a known extension")
+
     g.Describe(".command_for_file", func() {
       g.Describe("when a filename is given with a known extension", func() {
         g.It("should be a valid command", func() {
@@ -22,14 +25,14 @@ func Test(t *testing.T) {
       g.Describe("when a filename is given without a known extension", func() {
         g.It("should return an error", func() {
           _, err := commandForFile("hello.unknown")
-          g.Assert(err).Equal(errors.New("run could not determine how to run this file because it does not have a known extension"))
+          g.Assert(err).Equal(extensionError)
         })
       })
 
       g.Describe("when a filename is given without any extension", func() {
         g.It("should return an error", func() {
           _, err := commandForFile("hello")
-          g.Assert(err).Equal(errors.New("run could not determine how to run this file because it does not have a known extension"))
+          g.Assert(err).Equal(extensionError)
         })
       })
     })
