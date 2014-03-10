61b30704086f299580f15ceab078eb8cabbb0a18
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-10 04:10:44 -0400

    Add initial tests

---
+++ run_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "testing"
+  . "github.com/franela/goblin"
+)
+
+func Test(t *testing.T) {
+  g := Goblin(t)
+
+  g.Describe("Run", func() {
+    g.Describe(".command_for_file", func() {
+      g.Context("when a filename is given with a known extension", func() {
+        g.It("should be a valid command", func() {
+          g.Assert(Run.command_for_file("hello.rb")).Equal("ruby hello.rb")
+        })
+      })
+
+      g.Context("when a filename is given without a known extension", func() {
+        g.It("should be nil", func() {
+          g.Assert(Run.command_for_file("hello.unknown")).Equal(nil)
+        })
+      })
+
+      g.Context("when a filename is given without any extension", func() {
+        g.It("should be nil", func() {
+          g.Assert(Run.command_for_file("hello")).Equal(nil)
+        })
+      })
+    })
+  })
+}
